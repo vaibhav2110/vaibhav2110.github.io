faa650167e7d0b310f53e7d8bf72e955b2bc05b6
Vaibhav <vaibhpraky@gmail.com>
2018-11-06 17:21:36 +0530

    Second change

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+function submitForm() {
+    event.preventDefault();
+    let req = {};
+    req.name = document.forms['contactForm'].elements['name'].value;
+    req.email = document.forms['contactForm'].elements['email'].value;
+    req.message = document.forms['contactForm'].elements['message'].value;
+    console.log(req);
+    $.ajax({
+        url: 'localhost:5000',
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(req),
+        processData: false,
+        success: function (data, textStatus, jQxhr) {
+            $('#response pre').html(JSON.stringify(data));
+            document.getElementsByClassName('success')[0].style.display = 'inline';
+            setTimeout(()=>{
+                document.getElementsByClassName('success')[0].style.display = 'none';
+            }, 4000);
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
